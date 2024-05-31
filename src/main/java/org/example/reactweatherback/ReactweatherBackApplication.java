cdbe436b8e993b549414a410036b4a2c44d29c2d
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-05-31 16:19:39 +0200

    Initial commit

---
+++ src/main/java/org/example/reactweatherback/ReactweatherBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.reactweatherback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactweatherBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactweatherBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/reactweatherback/ReactweatherBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.reactweatherback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactweatherBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
