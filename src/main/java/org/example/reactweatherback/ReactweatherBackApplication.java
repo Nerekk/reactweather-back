fe97c0f832d5149d7ad7ac9ecfddf98d0a0ffd59
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-05-31 16:59:11 +0200

    Creating admin account every server launch

--- src/main/java/org/example/reactweatherback/ReactweatherBackApplication.java
+++ src/main/java/org/example/reactweatherback/ReactweatherBackApplication.java
@@ -1,13 +1,35 @@
 package org.example.reactweatherback;
 
+import org.example.reactweatherback.Auth.AuthenticationService;
+import org.example.reactweatherback.Auth.RegisterRequest;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import static org.example.reactweatherback.User.Role.ADMIN;
+
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class ReactweatherBackApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReactweatherBackApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            AuthenticationService service
+    ) {
+        return args -> {
+            var admin = RegisterRequest.builder()
+                    .email("admin@mail.com")
+                    .password("password")
+                    .role(ADMIN)
+                    .build();
+            System.out.println("Admin token: " + service.register(admin).getAccessToken());
+        };
+    }
 }
