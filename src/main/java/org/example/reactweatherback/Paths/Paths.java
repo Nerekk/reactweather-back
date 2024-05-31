2c04d7130314732f2a9c2d7c6bab7cb3b4879d05
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-05-31 16:55:44 +0200

    Constant variables for endpoint paths

---
+++ src/main/java/org/example/reactweatherback/Paths/Paths.java
@@ -0,0 +1,9 @@
+package org.example.reactweatherback.Paths;
+
+public class Paths {
+    public static final String PATH = "/api/";
+
+    public static final String AUTH = PATH + "auth/";
+    public static final String USER = PATH + "user/";
+    public static final String ADMIN = PATH + "admin/";
+}
