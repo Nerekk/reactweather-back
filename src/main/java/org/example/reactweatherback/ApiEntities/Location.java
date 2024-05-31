e83037c8bad8180a2624bf319e00ce4d4c820943
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-05-31 16:54:39 +0200

    Location entity implementation

---
+++ src/main/java/org/example/reactweatherback/ApiEntities/Location.java
@@ -0,0 +1,31 @@
+package org.example.reactweatherback.ApiEntities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    private String name;
+
+    private String country;
+
+    private Double lon;
+
+    private Double lat;
+}
