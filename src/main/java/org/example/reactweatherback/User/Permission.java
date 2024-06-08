b28547abfc39cd2f033a9ef96d55f2d439df5490
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-06-08 17:33:54 +0200

    Added permissions for user

--- src/main/java/org/example/reactweatherback/User/Permission.java
+++ src/main/java/org/example/reactweatherback/User/Permission.java
@@ -10,6 +10,11 @@ public enum Permission {
     ADMIN_UPDATE("admin:update"),
     ADMIN_CREATE("admin:create"),
     ADMIN_DELETE("admin:delete"),
+
+    USER_READ("user:delete"),
+    USER_UPDATE("user:delete"),
+    USER_CREATE("user:delete"),
+    USER_DELETE("user:delete"),
     ;
 
     @Getter
