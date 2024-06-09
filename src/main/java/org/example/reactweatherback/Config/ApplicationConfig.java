ebb90fd079a16af2d5c7b420d79508c84dc50938
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-06-09 23:33:09 +0200

    Remove of redundant cors config

--- src/main/java/org/example/reactweatherback/Config/ApplicationConfig.java
+++ src/main/java/org/example/reactweatherback/Config/ApplicationConfig.java
@@ -58,17 +58,4 @@ public class ApplicationConfig {
     return new RestTemplate();
   }
 
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-      }
-    };
-  }
 }
