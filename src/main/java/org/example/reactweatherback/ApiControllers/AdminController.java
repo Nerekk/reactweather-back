f1f54f4144c2221b8cda23a9feb095c3b8a7c62f
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-06-09 23:38:16 +0200

    Admin methods and endpoints implementation

--- src/main/java/org/example/reactweatherback/ApiControllers/AdminController.java
+++ src/main/java/org/example/reactweatherback/ApiControllers/AdminController.java
@@ -6,13 +6,7 @@ import org.example.reactweatherback.ApiServices.AdminService;
 import org.example.reactweatherback.ApiDtos.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -45,8 +39,20 @@ public class AdminController {
         return "DELETE:: admin controller";
     }
 
-    @GetMapping(path = "users")
+    @GetMapping(path = "users/all")
     public ResponseEntity<List<UserResponse>> getAllUsersInfo() {
         return ResponseEntity.ok(adminService.getAllUsers());
+    }
+
+    @DeleteMapping(path = "users/delete/{id}")
+    public ResponseEntity<?> deleteUserById(@PathVariable("id") Integer id) {
+        adminService.deleteUserById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping(path = "users/role/{id}")
+    public ResponseEntity<?> swapRoleById(@PathVariable("id") Integer id) {
+        adminService.swapRoleById(id);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/org/example/reactweatherback/ApiServices/AdminService.java
+++ src/main/java/org/example/reactweatherback/ApiServices/AdminService.java
@@ -3,6 +3,7 @@ package org.example.reactweatherback.ApiServices;
 import org.example.reactweatherback.ApiEntities.User;
 import org.example.reactweatherback.ApiRepos.UserRepository;
 import org.example.reactweatherback.ApiDtos.UserResponse;
+import org.example.reactweatherback.User.Role;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,15 +12,34 @@ import java.util.List;
 @Service
 public class AdminService {
     private final UserRepository userRepository;
+    private final UserService userService;
 
-    public AdminService(UserRepository userRepository) {
+    public AdminService(UserRepository userRepository, UserService userService) {
         this.userRepository = userRepository;
+        this.userService = userService;
     }
 
     public List<UserResponse> getAllUsers() {
         List<User> users = userRepository.findAll();
         List<UserResponse> responses = new ArrayList<>();
-        users.forEach((user -> responses.add(new UserResponse(user.getId(), user.getEmail(), user.getPassword()))));
+        users.forEach((user -> responses.add(new UserResponse(user.getId(), user.getEmail(), user.getPassword(), user.getRole(), userService.getLocations(user.getId())))));
         return responses;
+    }
+
+    public void deleteUserById(Integer userId) {
+        userRepository.deleteById(userId);
+    }
+
+    public void swapRoleById(Integer id) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) return;
+
+        Role role = user.getRole();
+        if (role == Role.ADMIN) {
+            user.setRole(Role.USER);
+        } else {
+            user.setRole(Role.ADMIN);
+        }
+        userRepository.save(user);
     }
 }
