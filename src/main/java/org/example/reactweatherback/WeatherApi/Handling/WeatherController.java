02e56c249c02aae7795e01fff19a059364cb337e
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-06-11 20:10:30 +0200

    Remove of redundant endpoints

--- src/main/java/org/example/reactweatherback/WeatherApi/Handling/WeatherController.java
+++ src/main/java/org/example/reactweatherback/WeatherApi/Handling/WeatherController.java
@@ -14,21 +14,6 @@ import org.springframework.web.bind.annotation.*;
 public class WeatherController {
     private final WeatherService weatherService;
 
-    @GetMapping(path = "current/{city}")
-    public WeatherResponse getCurrentWeather(@PathVariable("city") String city) {
-        return weatherService.getWeather(city);
-    }
-
-    @GetMapping(path = "forecast/{city}")
-    public ForecastResponse getForecastWeather(@PathVariable("city") String city) {
-        return weatherService.getForecast(city);
-    }
-
-    @GetMapping(path = "air/{lon}/{lat}")
-    public AirResponse getAirQuality(@PathVariable("lon") Double lon, @PathVariable("lat") Double lat) {
-        return weatherService.getAirPollution(lat, lon);
-    }
-
     @GetMapping(path = "combined/{city}")
     public CombinedWeatherResponse getCombinedWeather(@PathVariable("city") String city) {
         return weatherService.getCombined(city);
